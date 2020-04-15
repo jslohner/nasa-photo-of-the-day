022c3ce3305aec62ae787029ecab887246f0bb39
Justin Lohner <lohner4@gmail.com>
2020-04-15 17:55:10 -0600

    Wrote component for the photo section

---
+++ src/components/PhotoSection/Photo.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './PhotoSection.css';
+
+export default function Photo(props) {
+
+	const buttonHandler = () => {
+		if(!props.photoButton) {
+			props.setPhoto(props.hdurl);
+			props.setPhotoButton(true);
+		}
+		else {
+			props.setPhoto(props.url);
+			props.setPhotoButton(false);
+		}
+	}
+
+	return (
+		<div className='photo-section'>
+			<img className='photo' src={props.photo} alt='see explanation for the NASA APOD'/>
+			<button onClick={buttonHandler} className='button'>High Resolution Picture</button>
+			<button onClick={buttonHandler} className='button'>Low Resolution Picture</button>
+		</div>
+	);
+}
